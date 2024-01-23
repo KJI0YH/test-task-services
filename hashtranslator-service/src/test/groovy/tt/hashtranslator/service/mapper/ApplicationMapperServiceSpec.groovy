55c91ff86b315b515bf05b79980fe8dd3fd7fdbf
KJI0YH <aliaksei.kryzhanouski@gmail.com>
2024-01-23 18:00:38 +0300

    add tests for application mapper service

---
+++ hashtranslator-service/src/test/groovy/tt/hashtranslator/service/mapper/ApplicationMapperServiceSpec.groovy
@@ -0,0 +1,64 @@
+package tt.hashtranslator.service.mapper
+
+
+import org.spockframework.spring.EnableSharedInjection
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
+import spock.lang.Shared
+import spock.lang.Specification
+import tt.hashtranslator.dto.ApplicationRequestDto
+import tt.hashtranslator.entity.Application
+import tt.hashtranslator.entity.HashStatus
+import tt.hashtranslator.exception.MapperException
+
+@SpringBootTest
+@EnableSharedInjection
+@TestPropertySource(properties = "spring.config.location=classpath:application-test.yml")
+class ApplicationMapperServiceSpec extends Specification {
+
+    @Value('${service.hash-translator.hashes.max}')
+    @Shared
+    private int maxHashes
+
+    @Autowired
+    private ApplicationMapperService mapperService;
+
+    def "dtoToEntity should convert valid ApplicationRequestDto to Application entity"() {
+        given:
+        ApplicationRequestDto applicationRequestDto = new ApplicationRequestDto()
+        applicationRequestDto.setHashes(hashes)
+
+        when:
+        Application application = mapperService.dtoToEntity(applicationRequestDto)
+
+        then:
+        application.hashes.size() == hashes.size()
+        application.hashes.every { it.status == HashStatus.ACCEPTED }
+        where:
+        hashes << [
+                ["cfcd208495d565ef66e7dff9f98764da"],
+                (1..maxHashes).collect { "1" * 32 }
+        ]
+    }
+
+    def "dtoToEntity should throw MapperException invalid ApplicationRequestDto"() {
+        given:
+        ApplicationRequestDto applicationRequestDto = new ApplicationRequestDto()
+        applicationRequestDto.setHashes(hashes)
+
+        when:
+        mapperService.dtoToEntity(applicationRequestDto)
+
+        then:
+        thrown(MapperException)
+
+        where:
+        hashes << [
+                [],
+                ["invalid"],
+                (0..maxHashes).collect { "1" * 32 }
+        ]
+    }
+}
